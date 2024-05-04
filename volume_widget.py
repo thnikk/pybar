949ab6247e6d072163965d75039aa51787960561
thnikk <thnikkshop@gmail.com>
2024-05-04 15:21:35 -0700

    Show more info in PoC volume widget

--- volume_widget.py
+++ volume_widget.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3 -u
 """
-Description:
-Author:
+Description: Volume widget
+Author: thnikk
 """
 import common as c
 from pulse import Pulse
@@ -22,6 +22,8 @@ def volume_widget():
         for sink in p.get_sinks('sinks')
     }
 
+    section_box = c.box('v', spacing=10)
+    section_box.add(c.label('Outputs', style='title', ha='start'))
     icons = {'pnp': '', 'arctis': ''}
     sinks_box = c.box('v', style='box')
     for description, info in sinks.items():
@@ -30,10 +32,33 @@ def volume_widget():
                 sink_box = c.box('h', spacing=10, style='inner-box')
                 sink_box.add(c.label(icon, style='icon-volume'))
                 level = c.level(p.get_sink_volume(info['name']))
-                # sinks[description]['level'] = level
-                sink_box.pack_start(level, 1, 1, 0)
+                level_box = c.box('v')
+                level_box.pack_start(level, 1, 0, 0)
+                sink_box.pack_start(level_box, 1, 1, 0)
                 sinks_box.add(sink_box)
-    main_box.add(sinks_box)
+    section_box.add(sinks_box)
+    main_box.add(section_box)
+
+    section_box = c.box('v', spacing=10)
+    section_box.add(c.label('Programs', style='title', ha='start'))
+    sinks_box = c.box('v', style='box')
+    sink_inputs = {
+        sink['node.name']:
+        int(sink['Volume'].split('/')[1].split('%')[0].strip())
+        for sink in p.get_sinks('sink-inputs')
+        if 'node.name' in list(sink)
+    }
+    for name, volume in sink_inputs.items():
+        print(name, volume)
+        sink_box = c.box('v', spacing=10, style='inner-box')
+        sink_box.add(c.label(name))
+        level = c.level(volume)
+        sink_box.pack_start(level, 1, 1, 0)
+        sinks_box.add(sink_box)
+        if name != list(sink_inputs)[-1]:
+            sinks_box.add(c.sep('v'))
+    section_box.add(sinks_box)
+    main_box.add(section_box)
 
     # def get_volume(sinks):
     #     p = Pulse
