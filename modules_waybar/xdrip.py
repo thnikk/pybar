719ef5b05da580ad62e85e04918ce0631db6488e
thnikk <thnikkshop@gmail.com>
2024-05-15 20:46:37 -0700

    Move xdrip settings to config

--- modules_waybar/xdrip.py
+++ modules_waybar/xdrip.py
@@ -25,48 +25,33 @@ def get_config() -> configparser.ConfigParser:
     return config
 
 
-def print_error_to_bar(string):
-    """ Print an error to the bar and exit """
-    print(json.dumps({"text": string}))
-    sys.exit(1)
-
-
-def config_check(config):
-    """ Check config file for values """
-    check_values = ['api_secret', 'ip', 'port']
-    for key in list(config['settings']):
-        check_values.remove(key)
-    if check_values:
-        print_error_to_bar(
-            f"Set {' and '.join(check_values)} in ~/.config/beetus.ini"
-        )
-        sys.exit(1)
-
-
 def get_data(config):
     """ Get new data from API """
     # Build header for http request with api key
-    h = {"api-secret": config['settings']['api_secret']}
+    h = {"api-secret": config['api_secret']}
     cache = Cache(os.path.expanduser("~/.cache/beetus.json"))
     try:
-        data = requests.get((f"http://{config['settings']['ip']}:"
-                            f"{config['settings']['port']}/sgv.json"),
+        data = requests.get((f"http://{config['ip']}:"
+                            f"{config['port']}/sgv.json"),
                             headers=h, timeout=3).json()
         cache.save(data)
     except requests.exceptions.ConnectionError:
         try:
             data = cache.load()
         except FileNotFoundError:
-            print_debug('Something is fucked up, bruh.')
-            print_error_to_bar("Couldn't connect to api or use cache file.")
+            print_debug("Couldn't connect to api or use cache file.")
             sys.exit(1)
     return data
 
 
-def module(_):
+def module(config):
     """ Module """
-    config = get_config()
-    config_check(config)
+    if (
+        "ip" not in list(config) or
+        "port" not in list(config) or
+        "api_secret" not in list(config)
+    ):
+        return None
 
     data = get_data(config)
 
