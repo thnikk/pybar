9f6dbcda68854cc08018e84208c4a0afa2aa3e7f
thnikk <thnikkshop@gmail.com>
2024-06-03 14:03:12 -0700

    Created class for switch module and loaded options from config

--- modules/toggle.py
+++ modules/toggle.py
@@ -4,26 +4,59 @@ Description: Toggle module
 Author: thnikkk
 """
 import common as c
-from subprocess import run
+from subprocess import Popen, DEVNULL
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk, Gdk, GLib  # noqa
 
 
+class switch(Gtk.Box):
+    def __init__(self, icon, command):
+        super().__init__()
+        self.proc = None
+        self.icon = icon
+        self.command = command
+        self.set_spacing(5)
+        c.add_style(self, 'module')
+        self.add(c.label(icon))
+
+        switch_box = c.box('v')
+        self.switch = Gtk.Switch.new()
+        c.add_style(self.switch, 'switch')
+        switch_box.pack_start(self.switch, 1, 0, 0)
+        self.add(switch_box)
+        self.switch.connect('state_set', self.click_action)
+
+    def click_action(self, switch, state):
+        """ Launch or kill program """
+        if state:
+            self.proc = Popen(self.command, stdout=DEVNULL, stderr=DEVNULL)
+        else:
+            self.proc.terminate()
+
+
 def module(bar, config=None):
-    module = c.box('h', spacing=5)
-    c.add_style(module, 'module')
-    module.add(c.label(''))
-    # Put switch in vbox to prevent vertical expansion
-    switch_box = c.box('v')
-    switch = Gtk.Switch.new()
-    c.add_style(switch, 'switch')
-    switch_box.pack_start(switch, 1, 0, 0)
-    module.add(switch_box)
+    if not config:
+        config = {}
+    if 'icon' in list(config):
+        icon = config['icon']
+    else:
+        icon = ''
+    if 'command' in list(config):
+        command = config['command']
+    else:
+        command = ['tail', '-f', '/dev/null']
+    module = switch(icon, command)
 
     def update():
+        try:
+            if module.proc.poll() is not None:
+                module.switch.set_state(False)
+                module.proc = None
+        except AttributeError:
+            pass
         return True
 
     if update():
-        GLib.timeout_add(1000, update)
+        GLib.timeout_add(500, update)
         return module
