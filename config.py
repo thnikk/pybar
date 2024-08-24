717f7c8a642428b6b130facfc06860d86463ee95
thnikk <thnikkshop@gmail.com>
2024-08-23 18:13:51 -0700

    Allow comments in config json

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ Author:
 """
 import os
 import json
+import re
 
 
 def load(path):
@@ -12,23 +13,24 @@ def load(path):
     config_path = os.path.expanduser(path)
     if not os.path.exists(config_path):
         os.makedirs(config_path)
-        default_config = {
-            "modules-left": ["workspaces", "privacy"],
-            "modules-center": ["updates"],
-            "modules-right": [
-                "weather", "volume", "network", "clock", "power"],
-            "modules": {
-                "weather": {
-                    "zip_code": "94102"
-                },
-                "updates": {
-                    "interval": 300
-                },
-                "network": {
-                    "always_show": True,
-                }
-            }
+        default_config = """// vim:ft=jsonc
+{
+    "modules-left": ["workspaces", "privacy"],
+    "modules-center": ["updates"],
+    "modules-right": [
+        "weather", "volume", "network", "clock", "power"],
+    "modules": {
+        "weather": {
+            "zip_code": "94102"
+        },
+        "updates": {
+            "interval": 300
+        },
+        "network": {
+            "always_show": True,
         }
+    }
+}"""
         with open(f"{config_path}/config.json", 'w', encoding='utf-8') as file:
             file.write(json.dumps(default_config, indent=4))
         return default_config
@@ -36,4 +38,7 @@ def load(path):
         os.path.expanduser(f'{config_path}/config.json'),
         'r', encoding='utf=8'
     ) as file:
-        return json.loads(file.read())
+        output = file.read()
+        output = re.sub(re.compile(r"/\*.*?\*/", re.DOTALL), "", output)
+        output = re.sub(re.compile(r"//.*?\n"), "", output)
+        return json.loads(output)
