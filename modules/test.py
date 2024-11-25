75106c96e572504f79899656741290b459faea7d
thnikk <thnikkshop@gmail.com>
2024-11-25 15:03:19 -0800

    Added more useful tests to test module

--- modules/test.py
+++ modules/test.py
@@ -13,27 +13,30 @@ def module(bar, config=None):
     module = c.Module()
     module.set_position(bar.position)
     module.icon.set_label('')
-    module.text.set_label('0')
+    module.text.set_label('123')
 
     widget = c.Widget()
     widget.heading('Test')
+    widget.box.add(c.label('test', style='title', va='start'))
     widget.draw()
     module.set_popover(widget)
 
-    def click_action(module, event):
-        c.print_debug(event.button)
+    return module
 
-    def scroll_action(module, event):
-        c.print_debug(event.direction)
-
-    module.connect('button-press-event', click_action)
-    module.connect('scroll-event', scroll_action)
-
-    def update():
-        num = int(module.text.get_label()) + 1
-        module.text.set_label(str(num))
-        return True
-
-    if update():
-        GLib.timeout_add(1000, update)
-        return module
+    # def click_action(module, event):
+    #     c.print_debug(event.button)
+    #
+    # def scroll_action(module, event):
+    #     c.print_debug(event.direction)
+    #
+    # module.connect('button-press-event', click_action)
+    # module.connect('scroll-event', scroll_action)
+    #
+    # def update():
+    #     num = int(module.text.get_label()) + 1
+    #     module.text.set_label(str(num))
+    #     return True
+    #
+    # if update():
+    #     GLib.timeout_add(1000, update)
+    #     return module
