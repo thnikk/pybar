e0c7ac993b9f1afdda2c4860cc5f68c418061d6a
thnikk <thnikkshop@gmail.com>
2026-01-25 22:41:15 -0800

    Fixed output

--- main.py
+++ main.py
@@ -22,7 +22,24 @@ import module
 import gi
 gi.require_version('Gtk', '4.0')
 gi.require_version('Gtk4LayerShell', '1.0')
-from gi.repository import Gtk  # noqa
+from gi.repository import Gtk, Gio  # noqa
+
+
+class StreamToLogger:
+    """ Redirect a stream (stdout/stderr) to a logger """
+    def __init__(self, logger, log_level, stream):
+        self.logger = logger
+        self.log_level = log_level
+        self.stream = stream
+
+    def write(self, buf):
+        if buf.strip():
+            for line in buf.rstrip().splitlines():
+                self.logger.log(self.log_level, line.rstrip())
+        self.stream.write(buf)
+
+    def flush(self):
+        self.stream.flush()
 
 
 def setup_logging():
@@ -41,12 +58,42 @@ def setup_logging():
             logging.StreamHandler(sys.stderr)
         ]
     )
+
+    # Redirect stdout and stderr to logger
+    stdout_logger = logging.getLogger('STDOUT')
+    stderr_logger = logging.getLogger('STDERR')
+    sys.stdout = StreamToLogger(stdout_logger, logging.INFO, sys.stdout)
+    sys.stderr = StreamToLogger(stderr_logger, logging.ERROR, sys.stderr)
+
     # Suppress verbose connection pool logging
     logging.getLogger("urllib3").setLevel(logging.WARNING)
     return log_file
 
 
 def on_activate(app, config):
+    if hasattr(app, 'started') and app.started:
+        return
+    app.started = True
+
+    # Get a set of all used modules
+    unique = set(
+        config['modules-left'] +
+        config['modules-center'] +
+        config['modules-right']
+    )
+
+    # Start module threads
+    for name in unique:
+        # Load the module config if it exists
+        module_config = config['modules'].get(name, {})
+
+        # Set the interval if it's not specified
+        if 'interval' not in module_config:
+            module_config['interval'] = 5
+
+        # Start the worker thread for this module
+        module.start_worker(name, module_config)
+
     try:
         app.display = Display(config, app)
         # Draw all bars
@@ -61,6 +108,10 @@ def parse_args():
     parser = argparse.ArgumentParser(exit_on_error=False)
     parser.add_argument('-C', '--config', type=str, default='~/.config/pybar',
                         help="Configuration path")
+    parser.add_argument('-n', '--new', action='store_true',
+                        help="Allow multiple instances")
+    parser.add_argument('-r', '--replace', action='store_true',
+                        help="Replace existing instance")
     args, unknown = parser.parse_known_args()
     return args
 
@@ -73,33 +124,25 @@ def main():
     args = parse_args()
     config = Config.load(args.config)
 
-    # Get a set of all used modules
-    unique = set(
-        config['modules-left'] +
-        config['modules-center'] +
-        config['modules-right']
-    )
-
     # Set the cache directory if it's not specified in the config
     if "cache" not in list(config):
         config["cache"] = '~/.cache/pybar'
 
-    # Start module threads
-    for name in unique:
-        # Load the module config if it exists
-        module_config = config['modules'].get(name, {})
-
-        # Set the interval if it's not specified
-        if 'interval' not in module_config:
-            module_config['interval'] = 5
-
-        # Start the worker thread for this module
-        module.start_worker(name, module_config)
-
     # Create display object
-    app = Gtk.Application(application_id='org.thnikk.pybar')
+    flags = Gio.ApplicationFlags.ALLOW_REPLACEMENT
+    if args.new:
+        flags |= Gio.ApplicationFlags.NON_UNIQUE
+    if args.replace:
+        flags |= Gio.ApplicationFlags.REPLACE
+
+    app = Gtk.Application(
+        application_id='org.thnikk.pybar',
+        flags=flags
+    )
     app.connect('activate', lambda app: on_activate(app, config))
-    app.run()
+    
+    # Use an empty list for argv to prevent GTK from parsing custom args
+    app.run([])
 
 
 if __name__ == "__main__":
