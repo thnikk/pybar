718d0a6463e66f43331a64b07d6effb9ae2ab56b
thnikk <thnikkshop@gmail.com>
2026-01-26 23:12:59 -0800

    Fixed git module and collapsed commit info in list

--- modules/git.py
+++ modules/git.py
@@ -12,44 +12,56 @@ import gi
 gi.require_version('Gtk', '4.0')
 from gi.repository import Gtk  # noqa
 
+
 def get_repo_name(path):
     try:
-        output = run(['git', '-C', path, 'config', '--get', 'remote.origin.url'], 
-                     check=True, capture_output=True).stdout.decode('utf-8')
+        output = run(
+            ['git', '-C', path, 'config', '--get', 'remote.origin.url'],
+            check=True, capture_output=True).stdout.decode('utf-8')
         name = output.strip().split('/')[-1].split('.')[0].replace('-', ' ')
         return name.capitalize()
     except Exception:
         return os.path.basename(path).capitalize()
 
+
 def get_commits(path):
     try:
         run(['git', '-C', path, 'fetch'], check=True, capture_output=True)
-        
+
         # Try main then master
         cmd = ['git', '-C', path, 'log', '--name-only']
         try:
-            res = run(cmd + ['main..origin'], check=True, capture_output=True).stdout.decode('utf-8')
+            res = run(cmd + ['main..origin'], check=True,
+                      capture_output=True).stdout.decode('utf-8')
         except CalledProcessError:
-            res = run(cmd + ['master..origin'], check=True, capture_output=True).stdout.decode('utf-8')
-            
+            res = run(cmd + ['master..origin'], check=True,
+                      capture_output=True).stdout.decode('utf-8')
+
         output = {}
         chash = None
         for line in res.splitlines():
             if re.match('^commit', line):
                 chash = line.split()[1][:7]
-                output[chash] = {"author": None, "date": None, "msg": None, "files": []}
+                output[chash] = {"author": None,
+                                 "date": None, "msg": None, "files": []}
             elif re.match('^Author:', line) and chash:
-                output[chash]['author'] = line.split(":")[1].split("<")[0].strip()
+                output[chash]['author'] = line.split(
+                    ":")[1].split("<")[0].strip()
             elif re.match('^Date:', line) and chash:
                 date_str = ':'.join(line.split(":")[1:]).strip()
                 dt = datetime.strptime(date_str, '%a %b %d %H:%M:%S %Y %z')
                 delta = datetime.now(timezone.utc) - dt
                 if delta.days > 0:
-                    output[chash]['date'] = f"{delta.days} day{'s' if delta.days > 1 else ''} ago"
+                    output[chash]['date'] = \
+                        f"{delta.days} day{'s' if delta.days > 1 else ''} ago"
                 elif delta.seconds // 3600 > 0:
-                    output[chash]['date'] = f"{delta.seconds // 3600} hour{'s' if delta.seconds // 3600 > 1 else ''} ago"
+                    output[chash]['date'] = (
+                        f"{delta.seconds // 3600} hour"
+                        f"{'s' if delta.seconds // 3600 > 1 else ''} ago")
                 else:
-                    output[chash]['date'] = f"{delta.seconds // 60} minute{'s' if delta.seconds // 60 > 1 else ''} ago"
+                    output[chash]['date'] = (
+                        f"{delta.seconds // 60} minute"
+                        f"{'s' if delta.seconds // 60 > 1 else ''} ago")
             elif re.match('^ ', line) and chash:
                 output[chash]['msg'] = line.strip().replace('&', 'and')
             elif line and chash:
@@ -58,13 +70,14 @@ def get_commits(path):
     except Exception:
         return {}
 
+
 def fetch_data(config):
     path = os.path.expanduser(config.get('path', '~/'))
     icon = config.get('icon', '')
-    
+
     commits = get_commits(path)
     name = get_repo_name(path)
-    
+
     return {
         "text": f"{icon} {len(commits)}" if commits else "",
         "name": name,
@@ -73,18 +86,27 @@ def fetch_data(config):
         "icon": icon
     }
 
+
 def create_widget(bar, config):
     module = c.Module()
     module.set_position(bar.position)
     module.set_visible(False)
     return module
 
+
 def update_ui(module, data):
-    module.text.set_label(data['text'])
+    module.set_label(data['text'])
     module.set_visible(bool(data['text']))
-    
+
     if not module.get_active():
         module.set_widget(build_popover(data))
+
+
+def toggle_details(btn, details_box, indicator):
+    visible = not details_box.get_visible()
+    indicator.set_text('' if visible else '')
+    details_box.set_visible(visible)
+
 
 def build_popover(data):
     main_box = c.box('v', spacing=20)
@@ -93,32 +115,63 @@ def build_popover(data):
 
     commits_box = c.box('v', spacing=10)
     scroll_holder = c.box('v', spacing=10)
-    
+
     for chash, info in data['commits'].items():
-        commit_box = c.box('v', style='box')
-        
-        title_box = c.box('h', style='inner-box', spacing=20)
-        title = c.label(info['msg'], length=30, ha='start')
-        title.props.tooltip_text = info['msg']
-        title_box.append(title)
-        title_box.append(c.label(info['date'], ha='end', he=True))
-        commit_box.append(title_box)
+        item_con = c.box('v')
 
-        commit_box.append(c.sep('h'))
+        btn = c.button()
+        c.add_style(btn, ['minimal', 'inner-box'])
 
-        file_box = c.box('v', style='inner-box')
-        for f in info['files'][:5]: # Limit files in UI
-            file_box.append(c.label(f, ha='start'))
-        if len(info['files']) > 5:
-            file_box.append(c.label(f"...and {len(info['files'])-5} more", style='gray'))
-        commit_box.append(file_box)
+        btn_content = c.box('h', spacing=10)
+        indicator = c.label('', style='gray')
 
-        bottom_box = c.box('h', style='inner-box')
-        bottom_box.append(c.label(info['author'], ha='start'))
-        bottom_box.append(c.label(chash, ha='end', he=True))
-        commit_box.append(bottom_box)
+        # Message and date in the header
+        msg_label = c.label(info['msg'], length=30, ha='start', he=True)
+        msg_label.props.tooltip_text = info['msg']
 
-        scroll_holder.append(commit_box)
+        btn_content.append(indicator)
+        btn_content.append(msg_label)
+        btn_content.append(c.label(info['date'], ha='end'))
+
+        btn.set_child(btn_content)
+
+        # Container for details
+        details_box = c.box('v')
+        details_box.set_visible(False)
+        c.add_style(details_box, 'expanded-status')
+
+        details_box.append(c.sep('h'))
+
+        inner_details = c.box('v', style='inner-box', spacing=10)
+
+        # Files list
+        file_box = c.box('v')
+        for f in info['files'][:10]:  # Show more files in expanded view
+            file_box.append(c.label(f, ha='start', style='small'))
+        if len(info['files']) > 10:
+            file_box.append(
+                c.label(f"...and {len(info['files'])-10} more", style='gray'))
+
+        inner_details.append(file_box)
+        inner_details.append(c.sep('h'))
+
+        # Author and Hash
+        bottom_box = c.box('h')
+        bottom_box.append(c.label(info['author'], ha='start', style='gray'))
+        bottom_box.append(c.label(chash, ha='end', he=True, style='gray'))
+        inner_details.append(bottom_box)
+
+        details_box.append(inner_details)
+
+        btn.connect('clicked', toggle_details, details_box, indicator)
+
+        item_con.append(btn)
+        item_con.append(details_box)
+
+        # Wrap the whole item in a box to match the look
+        outer_box = c.box('v', style='box')
+        outer_box.append(item_con)
+        scroll_holder.append(outer_box)
 
     if len(data['commits']) > 5:
         scroll = c.scroll(height=600)
@@ -126,7 +179,7 @@ def build_popover(data):
         commits_box.append(scroll)
     else:
         commits_box.append(scroll_holder)
-        
+
     main_box.append(commits_box)
 
     def on_update_clicked(btn):
