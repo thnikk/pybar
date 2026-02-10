71f770dfb081cd0317255184922475d218053980
thnikk <thnikkshop@gmail.com>
2026-02-09 18:22:28 -0800

    Don't redraw full widget and added more types to schema

--- modules/custom.py
+++ modules/custom.py
@@ -3,6 +3,7 @@
 Description: Waybar-compatible custom module for executing external scripts
 Author: thnikk
 """
+from gi.repository import Gtk, Pango
 import common as c
 from subprocess import run, CalledProcessError, TimeoutExpired, Popen
 import json
@@ -10,7 +11,6 @@ import os
 import shlex
 import gi
 gi.require_version('Gtk', '4.0')
-from gi.repository import Gtk, Pango
 
 
 class SchemaWidgetBuilder:
@@ -19,7 +19,7 @@ class SchemaWidgetBuilder:
     """
 
     @staticmethod
-    def build(schema):
+    def build(schema, updateable_widgets=None):
         """
         Build a widget from a schema definition.
         Schema format:
@@ -46,7 +46,8 @@ class SchemaWidgetBuilder:
             # Try to find widget type from keys
             for key in schema:
                 if key in ['box', 'vbox', 'hbox', 'label', 'button',
-                           'separator', 'scroll']:
+                           'separator', 'scroll', 'levelbar', 'pillbar',
+                           'graph', 'image', 'slider']:
                     widget_type = key
                     params = schema[key]
                     break
@@ -61,12 +62,12 @@ class SchemaWidgetBuilder:
             None
         )
         if builder_method:
-            return builder_method(params)
+            return builder_method(params, updateable_widgets)
 
         return None
 
     @staticmethod
-    def _build_box(params):
+    def _build_box(params, updateable_widgets=None):
         """ Build a box container """
         orientation = params.get('orientation', 'h')
         spacing = params.get('spacing', 0)
@@ -97,29 +98,37 @@ class SchemaWidgetBuilder:
         # Add children
         children = params.get('children', [])
         for child_schema in children:
-            child = SchemaWidgetBuilder.build(child_schema)
+            child = SchemaWidgetBuilder.build(child_schema, updateable_widgets)
             if child:
                 box.append(child)
 
         return box
 
     @staticmethod
-    def _build_vbox(params):
+    def _build_vbox(params, updateable_widgets=None):
         """ Build a vertical box """
         params['orientation'] = 'v'
-        return SchemaWidgetBuilder._build_box(params)
+        return SchemaWidgetBuilder._build_box(params, updateable_widgets)
 
     @staticmethod
-    def _build_hbox(params):
+    def _build_hbox(params, updateable_widgets=None):
         """ Build a horizontal box """
         params['orientation'] = 'h'
-        return SchemaWidgetBuilder._build_box(params)
+        return SchemaWidgetBuilder._build_box(params, updateable_widgets)
 
     @staticmethod
-    def _build_label(params):
+    def _build_label(params, updateable_widgets=None):
         """ Build a label widget """
         text = params.get('text', '')
         label = Gtk.Label(label=str(text))
+
+        # Store reference if widget has an ID
+        widget_id = params.get('id')
+        if widget_id and updateable_widgets is not None:
+            updateable_widgets[widget_id] = {
+                'widget': label,
+                'type': 'label'
+            }
 
         # Set properties
         if 'style' in params:
@@ -132,7 +141,7 @@ class SchemaWidgetBuilder:
             label.set_halign(c.align.get(params['ha'], Gtk.Align.FILL))
         if 'va' in params:
             label.set_valign(c.align.get(params['va'], Gtk.Align.FILL))
-        
+
         # Text alignment within the label (0.0=left, 0.5=center, 1.0=right)
         if 'xalign' in params:
             xalign_val = params['xalign']
@@ -146,7 +155,7 @@ class SchemaWidgetBuilder:
                 yalign_map = {'start': 0.0, 'center': 0.5, 'end': 1.0}
                 yalign_val = yalign_map.get(yalign_val, 0.5)
             label.set_yalign(yalign_val)
-        
+
         if 'wrap' in params:
             label.set_wrap(True)
             if isinstance(params['wrap'], int):
@@ -159,10 +168,18 @@ class SchemaWidgetBuilder:
         return label
 
     @staticmethod
-    def _build_button(params):
+    def _build_button(params, updateable_widgets=None):
         """ Build a button widget """
         label_text = params.get('label', '')
         button = Gtk.Button(label=label_text)
+
+        # Store reference if widget has an ID
+        widget_id = params.get('id')
+        if widget_id and updateable_widgets is not None:
+            updateable_widgets[widget_id] = {
+                'widget': button,
+                'type': 'button'
+            }
 
         # Set properties
         if 'style' in params:
@@ -177,7 +194,7 @@ class SchemaWidgetBuilder:
         return button
 
     @staticmethod
-    def _build_separator(params):
+    def _build_separator(params, updateable_widgets=None):
         """ Build a separator widget """
         orientation = params.get('orientation', 'h')
 
@@ -196,7 +213,7 @@ class SchemaWidgetBuilder:
         return separator
 
     @staticmethod
-    def _build_scroll(params):
+    def _build_scroll(params, updateable_widgets=None):
         """ Build a scrolled window """
         width = params.get('width', 0)
         height = params.get('height', 0)
@@ -226,11 +243,135 @@ class SchemaWidgetBuilder:
         # Add child if present
         children = params.get('children', [])
         if children:
-            child = SchemaWidgetBuilder.build(children[0])
+            child = SchemaWidgetBuilder.build(children[0], updateable_widgets)
             if child:
                 window.set_child(child)
 
         return window
+
+    @staticmethod
+    def _build_levelbar(params, updateable_widgets=None):
+        """ Build a level bar widget """
+        min_val = params.get('min', 0)
+        max_val = params.get('max', 100)
+        value = params.get('value', 0)
+
+        levelbar = Gtk.LevelBar.new_for_interval(min_val, max_val)
+        levelbar.set_value(value)
+
+        # Store reference if widget has an ID
+        widget_id = params.get('id')
+        if widget_id and updateable_widgets is not None:
+            updateable_widgets[widget_id] = {
+                'widget': levelbar,
+                'type': 'levelbar'
+            }
+
+        if 'style' in params:
+            c.add_style(levelbar, params['style'])
+
+        return levelbar
+
+    @staticmethod
+    def _build_pillbar(params, updateable_widgets=None):
+        """ Build a pill bar widget """
+        height = params.get('height', 12)
+        radius = params.get('radius', 6)
+
+        pillbar = c.PillBar(height=height, radius=radius)
+
+        # Set initial segments if provided
+        segments = params.get('segments', [])
+        if segments:
+            pillbar.update(segments)
+
+        # Store reference if widget has an ID
+        widget_id = params.get('id')
+        if widget_id and updateable_widgets is not None:
+            updateable_widgets[widget_id] = {
+                'widget': pillbar,
+                'type': 'pillbar'
+            }
+
+        return pillbar
+
+    @staticmethod
+    def _build_graph(params, updateable_widgets=None):
+        """ Build a graph widget """
+        data = params.get('data', [])
+        height = params.get('height', 120)
+        width = params.get('width', 300)
+        min_val = params.get('min')
+        max_val = params.get('max')
+
+        graph = c.Graph(
+            data=data,
+            height=height,
+            width=width,
+            min_config=min_val,
+            max_config=max_val
+        )
+
+        # Store reference if widget has an ID
+        widget_id = params.get('id')
+        if widget_id and updateable_widgets is not None:
+            updateable_widgets[widget_id] = {
+                'widget': graph,
+                'type': 'graph'
+            }
+
+        return graph
+
+    @staticmethod
+    def _build_slider(params, updateable_widgets=None):
+        """ Build a slider widget """
+        value = params.get('value', 0)
+        min_val = params.get('min', 0)
+        max_val = params.get('max', 100)
+
+        slider = Gtk.Scale.new_with_range(
+            Gtk.Orientation.HORIZONTAL,
+            min_val,
+            max_val,
+            1
+        )
+        slider.set_value(value)
+        slider.set_draw_value(False)
+
+        # Store reference if widget has an ID
+        widget_id = params.get('id')
+        if widget_id and updateable_widgets is not None:
+            updateable_widgets[widget_id] = {
+                'widget': slider,
+                'type': 'slider'
+            }
+
+        if 'style' in params:
+            c.add_style(slider, params['style'])
+
+        return slider
+
+    @staticmethod
+    def _build_image(params, updateable_widgets=None):
+        """ Build an image widget """
+        file_path = params.get('path')
+        width = params.get('width')
+        height = params.get('height')
+
+        if file_path:
+            image = Gtk.Picture.new_for_filename(file_path)
+        else:
+            image = Gtk.Picture.new()
+
+        if width:
+            image.set_content_width(width)
+        if height:
+            image.set_content_height(height)
+
+        if 'style' in params:
+            c.add_style(image, params['style'])
+
+        return image
 
 
 class CustomModule(c.BaseModule):
@@ -238,11 +379,11 @@ class CustomModule(c.BaseModule):
 
     SCHEMA = {
         'exec': {
-            'type': 'string',
+            'type': 'file',
             'default': '',
             'label': 'Executable Script',
-            'description': 'Command to execute (supports arguments and '
-                           'quotes)'
+            'description': 'Path to script or command to execute '
+                           '(supports arguments and quotes)'
         },
         'return-type': {
             'type': 'choice',
@@ -409,15 +550,14 @@ class CustomModule(c.BaseModule):
         import weakref
         widget_ref = weakref.ref(m)
 
-        # Store widget schema for popover building
+        # Store widget schema and updateable widgets
         self._widget_schema = None
+        self._widget_schema_hash = None
+        self._updateable_widgets = {}
 
         def update_callback(data):
             widget = widget_ref()
             if widget is not None:
-                # Store widget schema if present
-                if 'widget' in data:
-                    self._widget_schema = data['widget']
                 self.update_ui(widget, data)
 
         sub_id = c.state_manager.subscribe(self.name, update_callback)
@@ -468,6 +608,10 @@ class CustomModule(c.BaseModule):
         if 'widget' in data and data['widget']:
             self._build_popover_widget(widget, data['widget'])
 
+        # Update widget values if present
+        if 'widget_updates' in data and self._updateable_widgets:
+            self._update_widget_values(data['widget_updates'])
+
         # Update CSS classes
         widget.reset_style()
         css_class = data.get('class', '')
@@ -478,25 +622,97 @@ class CustomModule(c.BaseModule):
         if data.get('stale'):
             c.add_style(widget, 'stale')
 
+    def _update_widget_values(self, updates):
+        """
+        Update widget values without rebuilding.
+        """
+        for widget_id, new_value in updates.items():
+            if widget_id in self._updateable_widgets:
+                widget_info = self._updateable_widgets[widget_id]
+                widget_obj = widget_info['widget']
+                widget_type = widget_info['type']
+
+                try:
+                    if widget_type == 'label':
+                        widget_obj.set_text(str(new_value))
+                    elif widget_type == 'button':
+                        widget_obj.set_label(str(new_value))
+                    elif widget_type == 'levelbar':
+                        widget_obj.set_value(float(new_value))
+                    elif widget_type == 'slider':
+                        widget_obj.set_value(float(new_value))
+                    elif widget_type == 'pillbar':
+                        widget_obj.update(new_value)
+                    elif widget_type == 'graph':
+                        widget_obj.update_data(new_value, None)
+                except Exception as e:
+                    c.print_debug(
+                        f"Custom module {self.name}: failed to update "
+                        f"widget {widget_id}: {e}",
+                        color='red'
+                    )
 
     def _build_popover_widget(self, module_widget, widget_schema):
         """
         Build a popover widget from schema definition.
+        Only rebuilds if schema changes.
         """
         try:
-            # Create popover widget
-            popover_widget = c.Widget()
+            # Calculate schema hash to detect changes
+            import hashlib
+            schema_str = json.dumps(
+                widget_schema.get('children', []),
+                sort_keys=True
+            )
+            schema_hash = hashlib.md5(schema_str.encode()).hexdigest()
 
-            # Build children from schema
-            children = widget_schema.get('children', [])
-            for child_schema in children:
-                child = SchemaWidgetBuilder.build(child_schema)
-                if child:
-                    popover_widget.box.append(child)
+            # Only rebuild if schema changed
+            if schema_hash != self._widget_schema_hash:
+                c.print_debug(
+                    f"Custom module {self.name}: building widget",
+                    color='green'
+                )
 
-            # Draw and attach to module
-            popover_widget.draw()
-            module_widget.set_popover(popover_widget)
+                # Get existing popover or create new one
+                popover_widget = module_widget.get_popover()
+                is_new = False
+                if not popover_widget:
+                    popover_widget = c.Widget()
+                    is_new = True
+                else:
+                    # Clear existing children
+                    child = popover_widget.box.get_first_child()
+                    while child:
+                        next_child = child.get_next_sibling()
+                        popover_widget.box.remove(child)
+                        child = next_child
+
+                # Clear previous updateable widgets
+                self._updateable_widgets = {}
+
+                # Build children from schema
+                children = widget_schema.get('children', [])
+                for child_schema in children:
+                    child = SchemaWidgetBuilder.build(
+                        child_schema,
+                        self._updateable_widgets
+                    )
+                    if child:
+                        popover_widget.box.append(child)
+
+                # Draw and attach if new
+                if is_new:
+                    popover_widget.draw()
+                    module_widget.set_popover(popover_widget)
+
+                # Store hash
+                self._widget_schema_hash = schema_hash
+
+                c.print_debug(
+                    f"Custom module {self.name}: widget built with "
+                    f"{len(self._updateable_widgets)} updateable widgets",
+                    color='green'
+                )
 
         except Exception as e:
             c.print_debug(
